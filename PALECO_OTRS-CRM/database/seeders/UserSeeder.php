a424374e7889d86943f64d31b96cc847a3b365d3
AllenFernandez <202380211@psu.palawan.edu.ph>
2026-06-29 18:35:07 +0800

    chore(db) Seeded fake admin account for debugging

---
+++ PALECO_OTRS-CRM/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'username' => 'allenglenn',
+            'first_name' => 'allen glenn',
+            'middle_name' => 'flojemon',
+            'last_name' => 'fernandez',
+            'email' => 'allenglennfernandez04@gmail.com',
+            'contact' => '09694547493',
+            'role' => 'admin',
+            'password' => 'password',
+        ]);
+    }
+}
