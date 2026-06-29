7896faf4d2f3dc8b1c5c73293d4bd3253e2cd4ed
AllenFernandez <202380211@psu.palawan.edu.ph>
2026-06-29 17:58:20 +0800

    chore(project) Prepared component files for User

--- PALECO_OTRS-CRM/app/Models/User.php
+++ PALECO_OTRS-CRM/app/Models/User.php
@@ -2,20 +2,21 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['username', 'first_name', 'middle_name', 'last_name', 'name_ext', 
+'email', 'contact', 'role', 'password', 'last_login'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasUlids;
 
     /**
      * Get the attributes that should be cast.
